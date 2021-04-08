2d384a1e8b552a14a3fe5d0d6debfd29bec92579
rudenko <alexey.rudenko@yola.com>
2021-04-08 12:27:53 +0300

    modify security groups, user-data template

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -31,6 +31,7 @@ data "template_file" "user_data" {
 
   vars = {
     backend_port = local.backend_port
+    cluster_name = var.cluster_name
     db_address = data.terraform_remote_state.db.outputs.address
     db_port = data.terraform_remote_state.db.outputs.port
   }
@@ -39,13 +40,15 @@ data "template_file" "user_data" {
 
 resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
+}
 
-  ingress {
-    from_port   = local.backend_port
-    to_port     = local.backend_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+resource "aws_security_group_rule" "allow_backend" {
+  type = "ingress"
+  security_group_id = aws_security_group.instance.id
+  from_port   = local.backend_port
+  to_port     = local.backend_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_launch_configuration" "example" {
@@ -102,22 +105,24 @@ resource "aws_lb_listener" "http" {
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
+}
 
-  # Allow all ingress
-  ingress {
-    from_port = local.http_port
-    to_port = local.http_port
-    protocol = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
 
-  # Allow outbound
-  egress {
-    from_port = local.any_port
-    to_port = local.any_port
-    protocol = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_securite_group.alb.id
+  from_port = local.any_port
+  to_port = local.any_port
+  protocol = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_lb_target_group" "asg" {
--- services/webserver-cluster/user-data.sh
+++ services/webserver-cluster/user-data.sh
@@ -2,6 +2,7 @@
 
 cat > index.html <<EOF
 <h1> Hello, Mr Alex!</h1>
+<p>ENV: ${cluster_name}</p>
 <p>DB address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
 EOF
